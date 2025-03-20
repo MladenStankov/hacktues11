c8622863553c7dd784d13ab17b4a1ef189cd75c7
ivanmatev1 <ivan_matev_@abv.bg>
2025-03-20 22:13:20 +0200

    fixed idNumber issue

--- app/actions/user.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-import prisma from "@/lib/prisma";
-
-export async function setIdNumber(userId: string, idNumber: string) {
-  await prisma.user.update({
-    where: { id: userId },
-    data: { idNumber },
-  });
-}
