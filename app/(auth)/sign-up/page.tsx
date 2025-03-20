0b9217bd3f96ecb09618b460ffcc0c0487867e97
ivanmatev1 <ivan_matev_@abv.bg>
2025-03-21 01:20:49 +0200

    added xml file to db

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,13 @@
+import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import React from 'react'
+
+export default function Page() {
+  return (
+    <Card>
+        <CardHeader>
+            <CardTitle>Sign Up</CardTitle>
+            <CardDescription>Sign up to your account</CardDescription>
+        </CardHeader>
+    </Card>
+  )
+}
